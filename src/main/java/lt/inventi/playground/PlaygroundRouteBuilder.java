2d228d807d6661132a2f134b402a26f7f2077a81
Norbert <norberto.kun@gmail.com>
2015-06-03 17:13:17 +0300

    Task #5 Completed
    Reiktų susikurti paprastą Ping Pong web servisą (serverio pusę). Jo
    WSDL'ą galima rasti čia:
    
    https://github.com/apache/ode/blob/master/axis2-war/src/test/resources/TestClusteredOutgoingTraffic/PingPong.wsdl
    Jį reikia įsidėti į src/main/resources direktoriją, application
    context'e apsirašyti CXF endpoint'ą, pvz:
    <cxf:cxfEndpoint id="pingPongEndpoint"
    address="/Ping"
    wsdlURL="PingPong.wsdl" endpointName="s:PingPong"
    serviceName="s:PingPongService"
    xmlns:s="http://pingpong.bpel.tps">
    <cxf:inInterceptors>
    <bean class="org.apache.cxf.transport.common.gzip.GZIPInInterceptor"/>
    </cxf:inInterceptors>
    <cxf:properties>
    <entry key="dataFormat" value="PAYLOAD"/>
    </cxf:properties>
    </cxf:cxfEndpoint>
    
    Ir atitinkamai apsibrėžti Camel route'ą:
    http://camel.apache.org/cxf.html.
    Čia užtenka, kad iškviestas servisas grąžintų tą, ką gavo (nors tai
    nebus validus atsakymas).
    Vietoje Spring XML'o galima naudoti Spring JavaConfig.
    Servisą reikia išsikviesti su Soap UI: http://www.soapui.org/.

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -1,44 +1,16 @@
 package lt.inventi.playground;
 
-import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.*;
-import org.apache.camel.processor.aggregate.AggregationStrategy;
-
-import java.util.ArrayList;
 
 /**
- * Task #3. Panaudoti Camel'io komponentą, kuris nuskaito ir rašo į failus.
- * Jeigu failas buvo tuščias, nieko nedaroma. Kitaip reikia failo turinį paversti
- * mažosiomis raidėmis, pridėti savo kažkokią signatūrą ir viską įrašyti į kitą
- * failą su "-result" priedėliu prie failo vardo (prieš failo išplėtimą).
- *
- * Užduočiai atlikti reikia kaip galima labiau naudoti Camel priemones, kur tik įmanoma (ir pavyks rasti).
- * Privaloma panaudoti "simple" (ir "file") kalbą.
+ * Fifth task
  */
+
 public class PlaygroundRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
-        from("file://files?noop=true")
-            .split(body(String.class).tokenize("\n"))
-            .aggregationStrategy(new AggregationStrategy() {
-                public Exchange aggregate(Exchange olde, Exchange newe) {
-                    if (olde == null) {
-                        newe.getIn().setBody(newe.getIn().getBody(String.class));
-//                        System.out.println(newe.getIn().getBody(String.class));
-                        return newe;
-                    }
-
-                    String oldExchange = olde.getIn().getBody(String.class);
-                    String newExchange = newe.getIn().getBody(String.class);
-                    olde.getIn().setBody(oldExchange + newExchange);
-//                    System.out.println(olde.getIn().getBody(String.class));
-                    return olde;
-                }
-            })
-            .setBody(body().prepend(simple("${property.CamelSplitIndex}++ ")))
-            .end()
-            .to("file://files/results?fileName=${file:name.noext}-result")
-        ;
+        from("cxf:bean:pingPongEndpoint")
+            .log(LoggingLevel.INFO, "${body}");
     }
 }
 
