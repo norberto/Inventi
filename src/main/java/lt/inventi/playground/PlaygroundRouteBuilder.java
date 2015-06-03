fae87fffd61f8f8d01c6de7d6655a53efca541b2
Norbert <norberto.kun@gmail.com>
2015-06-03 17:06:39 +0300

    Task #4 Completed
    Paimti duomenis iš failo, prie kiekvienos jo eilutės teksto prirašyti
    eilutės numerį (skaičiavimas prasideda nuo 1) ir galutinį rezultatą
    įrašyti į "result" failą (vardas kaip ir 3-ioje užduotyje). Viską
    padaryti Camel DSL priemonėmis be Processor'iaus ar Bean'o. Reikės
    panaudoti bent

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -1,7 +1,11 @@
 package lt.inventi.playground;
 
+import org.apache.camel.Exchange;
 import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.*;
+import org.apache.camel.processor.aggregate.AggregationStrategy;
+
+import java.util.ArrayList;
 
 /**
  * Task #3. Panaudoti Camel'io komponentą, kuris nuskaito ir rašo į failus.
@@ -14,13 +18,26 @@ import org.apache.camel.builder.*;
  */
 public class PlaygroundRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
-        from("file://files")
-            .filter(simple("${file:length} > 0"))
-                .convertBodyTo(String.class)
-                .transform(simple("${body.toLowerCase()}"))
-                .transform(simple("${body} \n--- I AM AMAZING --- "))
+        from("file://files?noop=true")
+            .split(body(String.class).tokenize("\n"))
+            .aggregationStrategy(new AggregationStrategy() {
+                public Exchange aggregate(Exchange olde, Exchange newe) {
+                    if (olde == null) {
+                        newe.getIn().setBody(newe.getIn().getBody(String.class));
+//                        System.out.println(newe.getIn().getBody(String.class));
+                        return newe;
+                    }
+
+                    String oldExchange = olde.getIn().getBody(String.class);
+                    String newExchange = newe.getIn().getBody(String.class);
+                    olde.getIn().setBody(oldExchange + newExchange);
+//                    System.out.println(olde.getIn().getBody(String.class));
+                    return olde;
+                }
+            })
+            .setBody(body().prepend(simple("${property.CamelSplitIndex}++ ")))
             .end()
-            .to("file://files/results?fileName=${file:name.noext}-result.${file:ext}")
+            .to("file://files/results?fileName=${file:name.noext}-result")
         ;
     }
 }
