590fa3ba4b3778e2f4528fd1872cb5dd099ab0d9
Norbert <norberto.kun@gmail.com>
2015-06-04 13:14:18 +0300

    Task #6 Part A FINAL update
    The correct version of this task.

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -1,8 +1,5 @@
 package lt.inventi.playground;
 
-import org.apache.camel.Exchange;
-import org.apache.camel.LoggingLevel;
-import org.apache.camel.Processor;
 import org.apache.camel.builder.*;
 import org.apache.camel.builder.xml.Namespaces;
 
@@ -13,16 +10,14 @@ import org.apache.camel.builder.xml.Namespaces;
 
 public class PlaygroundRouteBuilder extends RouteBuilder {
     Namespaces ns = new Namespaces("pin", "http://pingpong.bpel.tps");
+
     String prefix = "<pin:echoInputResponse xmlns:pin=\"http://pingpong.bpel.tps\">\n" + "<pin:echoInputReturn>";
     String postfix = "</pin:echoInputReturn>\n" + "</pin:echoInputResponse>\n";
 
     public void configure() throws Exception {
         from("cxf:bean:pingPongEndpoint")
-                .filter(ns.xpath("//pin:echoInput/pin:input"))
-                .setBody(ns.xpath("//pin:input/text()"))
-                .end()
-                .setBody(body().append(postfix).prepend(prefix))
-                .log(LoggingLevel.INFO, "\n\n${body}\n");
+            .setHeader("msg", ns.xpath("//pin:input/text()"))
+            .setBody(header("msg").prepend(prefix).append(postfix));
     }
 }
 
