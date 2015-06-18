e0700923a7956c4f6eccff48b1909ff33ec0c022
Norbert <norberto.kun@gmail.com>
2015-06-18 16:22:45 +0300

    Task #10 Completed
    10. Tas pats, kas ir 4 užduotyje, tik pirma rezultatų failas turi
    atsigulti kažkokioje ActiveMQ eilėje, o iš jos kitas "route'as" turi
    turinį įrašyti į "result" failą (pavadinimo sudarymo taisyklės išlieka).

--- src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
+++ src/main/java/lt/inventi/playground/PlaygroundRouteBuilder.java
@@ -8,36 +8,34 @@ import org.apache.camel.processor.aggregate.AggregationStrategy;
 import java.util.ArrayList;
 
 /**
- * Task #3. Panaudoti Camel'io komponentą, kuris nuskaito ir rašo į failus.
- * Jeigu failas buvo tuščias, nieko nedaroma. Kitaip reikia failo turinį paversti
- * mažosiomis raidėmis, pridėti savo kažkokią signatūrą ir viską įrašyti į kitą
- * failą su "-result" priedėliu prie failo vardo (prieš failo išplėtimą).
- *
- * Užduočiai atlikti reikia kaip galima labiau naudoti Camel priemones, kur tik įmanoma (ir pavyks rasti).
- * Privaloma panaudoti "simple" (ir "file") kalbą.
+ * Task #10. Tas pats, kas ir 4 užduotyje, tik pirma rezultatų failas turi atsigulti kažkokioje ActiveMQ eilėje,
+ * o iš jos kitas "route'as" turi turinį įrašyti į "result" failą (pavadinimo sudarymo taisyklės išlieka).
  */
 public class PlaygroundRouteBuilder extends RouteBuilder {
     public void configure() throws Exception {
-        from("file://files?noop=true")
+        from("file://files/?noop=true")
             .split(body(String.class).tokenize("\n"))
             .aggregationStrategy(new AggregationStrategy() {
                 public Exchange aggregate(Exchange olde, Exchange newe) {
                     if (olde == null) {
                         newe.getIn().setBody(newe.getIn().getBody(String.class));
-//                        System.out.println(newe.getIn().getBody(String.class));
+                      // System.out.println(newe.getIn().getBody(String.class));
                         return newe;
                     }
 
                     String oldExchange = olde.getIn().getBody(String.class);
                     String newExchange = newe.getIn().getBody(String.class);
                     olde.getIn().setBody(oldExchange + newExchange);
-//                    System.out.println(olde.getIn().getBody(String.class));
+                   // System.out.println(olde.getIn().getBody(String.class));
                     return olde;
                 }
             })
             .setBody(body().prepend(simple("${property.CamelSplitIndex}++ ")))
             .end()
-            .to("file://files/results?fileName=${file:name.noext}-result")
+            .to("activemq:queue:in");
+
+            from("activemq:queue:in")
+            .to("file://files/results/?fileName=${file:name.noext}-result");
         ;
     }
 }
